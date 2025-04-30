8a409755008865ad664596ca6185785eeb6f7262
Dan DeGenaro <daniel.degenaro@gmail.com>
2025-04-29 21:37:09 -0400

    init

---
+++ search.py
@@ -0,0 +1,183 @@
+import math
+import argparse
+from typing import Union
+
+import pandas as pd
+from pybktree import BKTree
+from strsimpy import (
+    Levenshtein,
+    WeightedLevenshtein,
+    Damerau,
+    OptimalStringAlignment,
+    LongestCommonSubsequence,
+    NGram,
+    QGram,
+)
+
+def fast_int_round(fn):
+    return lambda a, b: round(fn(a, b))
+
+def get_tree(name: str) -> BKTree:
+    
+    global docs
+    
+    tree = BKTree(
+        distance_func=rounded_dists[name]
+    )
+    l = len(docs['doc'])
+    for i, item in enumerate(docs['doc']):
+        tree.add(item)
+        if i % 1_000 == 0:
+            print(f'Loading tree \'{name}\'...{round(100 * i / l)}%', end='\r')
+    return tree
+
+def search(
+    name: str,
+    split: str,
+    min_R: int,
+    max_R: int,
+    max_K: int,
+    tree: BKTree = None
+):
+    
+    global train_queries
+    global test_queries
+    global docs
+    global doc_index
+    
+    if split == 'train':
+        queries = train_queries
+    elif split == 'test':
+        queries = test_queries
+    elif type(split) == pd.DataFrame:
+        queries = split
+        split = f'custom{len(split)}'
+        
+    if tree is None:
+        tree = get_tree(name)
+        
+    num_queries = len(queries)
+    log_freq = math.ceil(num_queries / 100)
+
+    run = []
+
+    for i, row in queries.iterrows():
+        
+        query_id = row['query_id']
+        query = row['query']
+        results = []
+        
+        previous_len = 0
+        for R in range(min_R, max_R + 1): # keep searching larger radii
+            more = tree.find(query, R)
+            more = more[previous_len:]
+            # print(len(more))
+            previous_len = len(more)
+            for j in range(1, len(more)):
+                score, doc = more[j]
+                doc_ids = doc_index.get(doc, [])
+                for doc_id in doc_ids:
+                    results.append({
+                        'query_id': query_id,
+                        'doc_id': doc_id,
+                        'score': 1. / score # inverse distance
+                    })
+                    if len(results) >= max_K:
+                        break
+                if len(results) >= max_K:
+                    break
+            if len(results) >= max_K:
+                break
+        results = results[:max_K]
+        
+        run.extend(results)
+            
+        if (i+1) % log_freq == 0:
+            print(f'Processed query {i+1}/{num_queries}...', end='\r')
+        
+    pd.DataFrame(run).to_json(
+        f'searches/{name}_{split}_R_{min_R}_{max_R}_K_{max_K}.jsonl',
+        lines=True,
+        orient='records'
+    )
+    
+if __name__ == '__main__':
+    
+    dists = {
+        'lev': Levenshtein().distance,
+        'wlev_ins': WeightedLevenshtein(
+            insertion_cost_fn=lambda char: 2
+        ).distance,
+        'wlev_del': WeightedLevenshtein(
+            deletion_cost_fn=lambda char: 2
+        ).distance,
+        'wlev_sub': WeightedLevenshtein(
+            substitution_cost_fn=lambda char_a, char_b: 2
+        ).distance,
+        'dam': Damerau().distance,
+        'osa': OptimalStringAlignment().distance,
+        'lcs': LongestCommonSubsequence().distance,
+        'ng1': NGram(1).distance,
+        'ng2': NGram(2).distance,
+        'ng3': NGram(3).distance,
+        'ng4': NGram(4).distance,
+        'qg1': QGram(1).distance,
+        'qg2': QGram(2).distance,
+        'qg3': QGram(3).distance,
+        'qg4': QGram(4).distance
+    }
+    
+    rounded_dists = {
+        name: fast_int_round(dist_fn) for name, dist_fn in dists.items()
+    }
+
+    for name in dists:
+        assert round(dists[name]('hello', 'hallo')) == rounded_dists[name]('hello', 'hallo')
+        
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--name',
+        '-n',
+        choices = list(dists.keys()),
+        required=True
+    )
+    parser.add_argument(
+        '--split',
+        '-s',
+        choices = ['train', 'test'],
+        required=True
+    )
+    parser.add_argument(
+        '--min_R',
+        '-m',
+        type = int,
+        default = 3
+    )
+    parser.add_argument(
+        '--max_R',
+        '-M',
+        type = int,
+        default = 10
+    )
+    parser.add_argument(
+        '--max_K',
+        '-k',
+        '-K',
+        type = int,
+        default = 100
+    )
+        
+    train_queries = pd.read_csv('queries-train.tsv', sep='\t')
+    test_queries = pd.read_csv('queries-test.tsv', sep='\t')
+    docs = pd.read_csv('docs.tsv', sep='\t')
+    doc_index = docs.groupby('doc')['doc_id'].unique().to_dict()
+    
+    args = parser.parse_args()
+    
+    search(
+        name = args.name,
+        split = args.split,
+        min_R = args.min_R,
+        max_R = args.max_R,
+        max_K = args.max_K
+    )
